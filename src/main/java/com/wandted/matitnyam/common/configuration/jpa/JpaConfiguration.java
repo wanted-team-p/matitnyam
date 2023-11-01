7aeeee7a4f7fb6ab657a3770ac83e62972351920
irerin07 <counterchord89@gmail.com>
2023-11-02 00:34:14 +0900

    #3 JpaConfig 추가 - JpaConfig 추가

---
+++ src/main/java/com/wandted/matitnyam/common/configuration/jpa/JpaConfiguration.java
@@ -0,0 +1,33 @@
+package com.wandted.matitnyam.common.configuration.jpa;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * @author 민경수
+ * @description jpa configuration
+ * @since 2023.11.02
+ **********************************************************************************************************************/
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EntityScan(basePackageClasses = {Jsr310JpaConverters.class}, basePackages = {"com.wandted.matitnyam.domain.entity"})
+@EnableJpaRepositories(basePackages = {"com.wandted.matitnyam.repository"})
+public class JpaConfiguration {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
