5501ac095e34ce3266b44e863e644d79428decee
irerin07 <counterchord89@gmail.com>
2023-11-02 19:19:26 +0900

    #6 맛집 데이터 조회 스케줄러 - 스케줄러

--- src/main/java/com/wandted/matitnyam/scheduler/RestaurantScheduler.java
+++ src/main/java/com/wandted/matitnyam/scheduler/RestaurantScheduler.java
@@ -1,9 +1,9 @@
 package com.wandted.matitnyam.scheduler;
 
 /**
- * @since       2023.11.02
- * @author      민경수
+ * @author 민경수
  * @description restaurant scheduler
+ * @since 2023.11.02
  **********************************************************************************************************************/
 public class RestaurantScheduler {
 }
