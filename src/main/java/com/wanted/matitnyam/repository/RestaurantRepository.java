fa1a9168fd9f4ec1546f24eb859bf51af9059cae
neppiness <0414kjh@naver.com>
2023-12-24 18:41:43 +0900

    Fix: RestaurantRepository의 불필요한 Repository 어노테이션 삭제

--- src/main/java/com/wanted/matitnyam/repository/RestaurantRepository.java
+++ src/main/java/com/wanted/matitnyam/repository/RestaurantRepository.java
@@ -2,8 +2,6 @@ package com.wanted.matitnyam.repository;
 
 import com.wanted.matitnyam.domain.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long>, RestaurantCustomRepository {
 }
